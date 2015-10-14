eddc5ec83cbd534174ab7faa9df465270eb9c79e
Koji Yamamoto <kowkow.y@gmail.com>
2015-10-14 22:12:07 +0900

    add config_default

---
+++ config_default.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_NAME', '');
+define('DATA_COLLECTION_NAME', '');
+define('QUEUE_COLLECTION_NAME', '');
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('ACCESS_TOKEN', '');
+define('ACCESS_TOKEN_SECRET', '');
+
+define('START_SCREEN_NAME', '');
